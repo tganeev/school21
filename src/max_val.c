274af973df8c3ccd5e886f97243d537ad2477539
Kindly Frostbite <kindlyfr@yc-c6.ufa.21-school.ru>
2026-05-21 16:15:07 +0500

    max_val done

---
+++ src/max_val.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int main() {
+    int n, i = 0;
+    int size = 1;
+    //int *arr = malloc(size * sizeof(int));
+    int max;
+    while(1) {
+        if(scanf("%d", &n) != 1) {
+            break;
+        }
+        if (n == -1) {
+            break;
+        }
+
+        if (i == 0) {
+            max = n;
+        }
+
+        if (max < n) {
+            max = n;
+        }
+        //arr[i] = n;
+        
+        //size++;
+        i++;
+        //arr = realloc(arr, size * sizeof(int));
+    }
+
+    printf("%d\n", max);
+
+
+
+}
