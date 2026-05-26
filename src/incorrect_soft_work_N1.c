13de767e3bf7e034a676ceb204b54753f0f30ed5
Kindly Frostbite <kindlyfr@yc-c6.ufa.21-school.ru>
2026-05-26 12:34:45 +0500

    fixed

---
+++ src/incorrect_soft_work_N1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 0;
+
+    scanf("%d", x);
+    printf("%05d", x);
+
+    return 0;
+}
+
+*/
+
+
+int main() {
+    int x = 0;
+    scanf("%d", &x);
+    printf("%05d", x);
+
+    return 0;
+}
