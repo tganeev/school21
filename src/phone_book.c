cdad4d6d945fd9c70d1fbea0ad3679ae9ad00614
Kindly Frostbite <kindlyfr@kk-c6.ufa.21-school.ru>
2026-06-17 12:56:44 +0500

    ver 2

--- src/phone_book.c
+++ src/phone_book.c
@@ -9,11 +9,17 @@
 
 typedef struct {
         char name[CONTACT][NAME];
-        int number[CONTACT][NUMBER];
+        int number[NUMBER];
     } t_contact;
 
 
 
+
+void menu() {
+    printf("1. Добавить контакт\n");
+    printf("2. Показать контакты\n");
+    printf("3. Выход\n");
+}
 
 
 void add_contact(char name[], char number[]) {
@@ -33,46 +39,15 @@ void show_contact() {
 
 int main() {
 
-    int n, flag = 1;
+    int n, flag = 1, count = 0, repeat = 1;
     char *name = malloc(50 * sizeof(char));
    
     char number[NUMBER];
 
     t_contact phone_book;
 
-    phone_book.name[0][0] = 'T';
-    phone_book.name[0][1] = 'i';
-    phone_book.name[0][2] = 'm';
-    phone_book.name[0][3] = 'u';
-    phone_book.name[0][4] = 'r';
-
-    phone_book.name[1][0] = 'I';
-    phone_book.name[1][1] = 'l';
-    phone_book.name[1][2] = 'd';
-    phone_book.name[1][3] = 'a';
-    phone_book.name[1][4] = 'r';
-
-    phone_book.number[0][0] = 5437;
-    phone_book.number[1][0] = 2443;
     
-    int j = 0;
-    for (int i = 0; i < 2; i++) {
-        
-             printf("%s %d\n", phone_book.name[i], phone_book.number[i][0]);
-             j++;
-        
-    }
-
-   
-
-
-
-
-    printf("1. Добавить контакт\n");
-    printf("2. Показать контакты\n");
-    printf("3. Выход\n");
-
-    
+    menu();
 
     while(flag) {
 
@@ -81,48 +56,60 @@ int main() {
         printf("Вы ввели не корректное значение\n");
         break;
     }
-    while (getchar() != '\n');    
+    //while (getchar() != '\n');    
         
     if (n == 1) {
-        printf("Введите имя: \n");
-        char chn;
         
+        while(repeat) {
+            while (getchar() != '\n');  
+
+        printf("Введите имя: \n");
+                
         for (int i = 0; i < NAME; i++) {
-            scanf("%c", &chn);
-            
-            name[i] = chn;
-            
-            
-            if (chn == '\n') {
-                printf("Имя записано\n");
-                //printf("%s\t", name);
+            phone_book.name[count][i] = getchar();
+            if (phone_book.name[count][i] == '\n') {
+                phone_book.name[count][i] = '\0';
+                //count++;
+                i = 0;
                 break;
             }
             
-            
-            
         }
-
+       
         printf("Введите телефон: \n");
-        char cht;
-        for (int i = 0; i < NUMBER; i++) {
-            scanf("%c", &cht);
-            //while (getchar() != '\n');
-            
-            if (cht == '\n') {
+       
 
-                printf("Телефон записан\n");
-                break;
+            scanf("%d", &phone_book.number[count++]);
+            
+            while (getchar() != '\n');   
+            
+       
+
+        printf("Хотите продолжить ввод?(y/n)\n");
+        char q = getchar();
+
+            if (q == 'y') {
+                repeat = 1;
             }
-            number[i] = cht;
+            else if (q == 'n') {
+                repeat = 0;
+                menu();
+            }
+
         }
+        
 
     }
         
     
     else if (n == 2) {
         
-            printf("Имя: %s Телефон: %s", name, number);
+            printf("Список контактов: \n");
+            for (int i = 0; i < count; i++) {
+                printf("Имя: %s | Телефон: %d\n", phone_book.name[i], phone_book.number[i]);
+            }
+
+            //menu();
         
     }
     else if (n == 3) {
