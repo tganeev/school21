7de98651df6b2e3b57d5a9b3cbadab31c6a016b4
Kindly Frostbite <kindlyfr@kk-c6.ufa.21-school.ru>
2026-07-04 15:37:16 +0500

    bicycle_rental v2

--- C/src/bicycle_rental.c
+++ C/src/bicycle_rental.c
@@ -17,6 +17,7 @@ int main()
     scanf("%d %d", &n, &t);
 
     int p = n;
+    int u = n;
 
     printf("Введенные данные: %d %d\n", n, t);
 
@@ -24,6 +25,8 @@ int main()
     count.a = malloc(n * sizeof(int*));
     count.f = malloc(n * sizeof(int*));
     count.s = malloc(n * sizeof(int*));
+
+    int **pivot = malloc(100 * sizeof(int*));
 
     int i = 0;
 
@@ -51,12 +54,63 @@ int main()
         }
 
     }
+    int x = 0;
+    int end = 0;
+    int bicycle = 0;
+    int z = t;
+    int w = t;
+    
 
+    while(z > 0) {
+        pivot[x] = malloc(2 * sizeof(int));
+        pivot[x][0] = x;
+        pivot[x][1] = end;
+        pivot[x][2] = bicycle;
+        x++;
+        z--;
+    }
+
+    int k = 0;
+    int sum = 0;
+       
+
+    for (int i = 0; i < w; i++) {
+        if (pivot[i][0] == count.a[k][0]) {
+            for (int j = i; j < count.f[k][0]; j++) {
+                pivot[j][1] = pivot[j][1] + 1;
+                pivot[j][2] += count.s[k][0];
+                if (pivot[j][2] > sum) {
+                    sum = pivot[j][2];
+                }
+            }
+            k++;
+            i = 0;
+        }
+           
+        if (k == u) {
+            break;
+        }
+        
+    }
+
+    printf("Пиковая загрузка: %d \n", sum);
+    /*
+    for (int i = 0; i < t; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", pivot[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    
+   
     if (pik > 0 && pik > max) {
         printf("%d\n", pik);
     } else {
         printf("%d\n", max);
     }
+    */
 
     
 
